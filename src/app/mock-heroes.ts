678098aa09990d4f91684d7253aac99fbbcd0d37
julyraining <julyraining@163.com>
2020-12-10 01:42:48 +0800

    init single page

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero'
+
+export const HEROES:Hero[] = [
+    {id:1,name:'Spike'},
+    {id:11, name:'Zeus'},
+    {id:12, name:'Someone'},
+    {id:13, name:'Fye'},
+    {id:14, name:'Ad'},
+    {id:15, name:'kathrine'},
+    {id:16, name:'Mike'},
+    {id:17, name:'Tank'},
+    {id:18, name:'Susan'},
+    {id:19, name:'Oliver'},
+]
